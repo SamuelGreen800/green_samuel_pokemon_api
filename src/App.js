e346ed9b4131ebf73250bc0efb621926c9beaecf
Samuel Green <samuelgreen800@tutanota.com>
2022-05-14 14:01:55 -0400

    PokeAPI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const App = (props) => {
+    const [pokes, setPokes] = useState([]);
+
+    useEffect(() => {
+        fetch('https://pokeapi.co/api/v2/pokemon-species/?limit=808&offset=01"')
+            .then(response => response.json())
+            .then(response => setPokes(response.results))
+    }, []);
+
+    return (
+        <div>
+          <h1>Hello!</h1>
+          
+            {pokes.length > 0 && pokes.map((pokes, index)=>{
+                return (<ul key={index}><li>{pokes.name}</li></ul>)
+            })}
+        </div>
+    );
 }
+
 
 export default App;
