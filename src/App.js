1f8900758a56716e1a1bfc9ac55d9793b977ce38
Samuel Green <samuelgreen800@tutanota.com>
2022-05-14 15:13:26 -0400

    V2 axios

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+
 
   const App = (props) => {
     const [pokes, setPokes] = useState([]);
 
     useEffect(() => {
-        fetch('https://pokeapi.co/api/v2/pokemon-species/?limit=808&offset=01"')
-            .then(response => response.json())
-            .then(response => setPokes(response.results))
+        axios
+          .get('https://pokeapi.co/api/v2/pokemon-species/?limit=808&offset=01')
+            .then(response => {setPokes(response.data.results)})
     }, []);
 
     return (
         <div>
           <h1>Hello!</h1>
-          
             {pokes.length > 0 && pokes.map((pokes, index)=>{
                 return (<ul key={index}><li>{pokes.name}</li></ul>)
             })}
